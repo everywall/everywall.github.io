813ac6087c7030271165c848d62964a57b905168
ladddder <151469127+ladddder@users.noreply.github.com>
2023-12-07 23:10:15 +0100

    replace default links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/everywall/everywall.github.io/tree/main/docs',
         },
         /*
         blog: {
@@ -75,7 +75,7 @@ const config: Config = {
         },
         //{to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/everywall/ladder',
           label: 'GitHub',
           position: 'right',
         },
@@ -98,7 +98,7 @@ const config: Config = {
           items: [
             {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              href: 'https://discord.gg/brSHjrkE',
             },
           ],
         },
