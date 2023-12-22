5b578ee0b55539a20ae21935506181ddfb790956
joncrangle <94425204+joncrangle@users.noreply.github.com>
2023-12-21 23:39:38 -0500

    Remove blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -115,10 +115,6 @@ const config: Config = {
           title: "More",
           items: [
             {
-              label: "Blog",
-              to: "/blog",
-            },
-            {
               label: "GitHub",
               href: "https://github.com/everywall/ladder",
             },
